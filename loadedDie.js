c7ff496c1d0c64fe6e54a07b0b3a5ad947378f2f
Adam Dipinto <adam.dipinto@gmail.com>
2016-11-03 23:30:29 +0000

    Added code from compass

---
+++ loadedDie.js
@@ -0,0 +1,10 @@
+// Lighthouse Labs - W1D4 - Closure Exercise //
+
+var loadedDie = (function () {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+})();
+
+console.log(loadedDie());  // 5
+console.log(loadedDie());  // 4
+console.log(loadedDie());  // 6
